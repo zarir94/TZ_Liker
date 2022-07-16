fa08ad59246eb58fae2c70cdc24494a1adf528fd
Technical Zarir <shahriarzariracc@gmail.com>
2022-07-16 14:39:50 +0600

    New Commit

--- TZ_Liker/urls.py
+++ TZ_Liker/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+PATH = '/files/'
+ROOT = settings.BASE_DIR / 'static'
 
 urlpatterns = [
     path('superuser/', admin.site.urls),
     path('', include('Home.urls')),
-]
+] + static(PATH, document_root=ROOT)
