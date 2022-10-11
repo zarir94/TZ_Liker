212ba6723951b12e2c2b46bb509e97c29de89cdf
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-11 21:52:23 +0600

    Update admin.py

--- Home/admin.py
+++ Home/admin.py
@@ -3,7 +3,7 @@ from .models import *
 # Register your models here.
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display=["has_cookie"]
+    list_display=["username","email","has_cookie"]
     list_filter=["has_cookie"]
 
 admin.site.register(Account, AccountAdmin)
