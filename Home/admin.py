24b2a7a7c121a365cce6a500c88b8313019eec05
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-11 19:41:09 +0600

    Update admin.py

--- Home/admin.py
+++ Home/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display=["has_cookie"]
+    list_filter=["has_cookie"]
+
+admin.site.register(Account, AccountAdmin)
 admin.site.register(Site_Info)
 admin.site.register(Contact)
 
